45c6b21e2f7a5095d6c7aa9a045f3dfb6eb02bad
joban <91016674+acrcw@users.noreply.github.com>
2023-07-01 22:35:35 +0530

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express") // return fnc for acess
 const socket = require("socket.io") // return function for initialization
 const app = express() // initialize app and server ready
-
+let port = process.env.PORT || 5000
 const server = app.listen(port);
 app.use(express.static("frontend")) // to show the index.html file
 
