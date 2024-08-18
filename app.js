c1ea9f495e9d50134bd53ed6b0aa1011eca2c04c
joban <91016674+acrcw@users.noreply.github.com>
2024-08-18 19:38:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express") // return fnc for acess
 const socket = require("socket.io") // return function for initialization
 const app = express() // initialize app and server ready
-let port = process.env.PORT || 10000
+let port = 80
 const server = app.listen(port);
 app.use(express.static("public")) // to show the index.html file
 
